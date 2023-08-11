a8863fa720fceb2e92f8b8273c54918279fe4bd9
onyinyeobidi <ezeobidiclementina@gmail.com>
2023-08-11 14:00:44 -0700

    5-print_numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -4,18 +4,15 @@
 
 /**
  * main - Entry point
- * Retun: Always 0 (Success)
+ * Return: always 0
  */
 
 int main(void)
-
 {
-
 	int i;
 
 	for (i = 0; i < 10; i++)
 	printf("%d", i);
 	printf("\n");
-
 	return (0);
 }
