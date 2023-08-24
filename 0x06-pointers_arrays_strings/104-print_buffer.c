922037058df67c5b69f5ae3fed2f1dbe7bd94394
onyinyeobidi <ezeobidiclementina@gmail.com>
2023-08-24 14:20:26 -0700

    104-print_buffer.c 2

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -7,9 +7,7 @@
  * @size: size
  * Return: void
  */
-
 void print_buffer(char *b, int size)
-
 {
 	int o, j, i;
 
@@ -47,6 +45,6 @@ void print_buffer(char *b, int size)
 			}
 		  printf("\n");
 			  o += 10;
+		}
 	}
-}
 }
