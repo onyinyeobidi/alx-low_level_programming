f7fe8ef2bf4f54f7ac6c6cea130b6d57917e5812
onyinyeobidi <ezeobidiclementina@gmail.com>
2023-08-30 13:44:06 -0700

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - check the code
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int g;
+
+	g = _strlen_recursion("Corbin Coleman");
+	printf("%d\n", g);
+	return (0);
+}
