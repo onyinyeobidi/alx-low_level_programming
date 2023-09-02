bdbe10195c3aeb70dcb2fa2718364e761983c9b8
onyinyeobidi <ezeobidiclementina@gmail.com>
2023-09-02 13:26:16 -0700

    first task from 0x09

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,10 +1,11 @@
 #include "main.h"
 /**
- * main - this is a main file
+ * main - this is a entry point of the file
+ * Retuen - always 0 (success)
+ * @_puts - what is beeing displayed
  */
 int main(void)
 {
 	_puts("\"my goal was to be the best hacker\"\n\t- Kevin Mitnick");
-
 	return (0);
 }
