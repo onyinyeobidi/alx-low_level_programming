0b60c4745dfcaa4928cc5450f06c6fb7af124a1e
onyinyeobidi <ezeobidiclementina@gmail.com>
2023-09-26 17:47:36 -0700

    more on singly linked list tsk 1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * listint_len -  This returns the number of elements in a linked lists
+ * @h: It is a linked list of type listint_t to traverse
+ * Return: number of nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t  val = 0;
+
+	while (h)
+	{
+		val++;
+		h = h->next;
+	}
+	return (val);
+}
