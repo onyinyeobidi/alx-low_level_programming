340ff26196cf14b98b14d048943356d8c3895e5d
onyinyeobidi <ezeobidiclementina@gmail.com>
2023-09-26 21:02:16 -0700

    more on singly linked list tsk 101

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,74 @@
+#include "lists.h"
+#include <stdio.h>
+size_t looped_listint_len(const listint_t *head);
+size_t print_listint_safe(const listint_t *head);
+/**
+ * looped_listint_len - Counts the number of unique nodes
+ * in a looped listint_t linked list.
+ * @head: A pointer to the head of the listint_t to check.
+ * Return: If the list is not looped - 0.
+ * Otherwise - the number of unique nodes in the list.
+ */
+size_t looped_listint_len(const listint_t *head)
+{
+	const listint_t *shoe, *hare;
+	size_t nodes = 1;
+
+	if (head == NULL || head->next == NULL)
+		return (0);
+	shoe = head->next;
+	hare = (head->next)->next;
+	while (hare)
+	{
+		if (shoe == hare)
+		{
+			shoe = head;
+			while (shoe != hare)
+			{
+				nodes++;
+				shoe = shoe->next;
+				hare = hare->next;
+			}
+			shoe = shoe->next;
+			while (shoe != hare)
+			{
+				nodes++;
+				shoe = shoe->next;
+			}
+			return (nodes);
+		}
+		shoe = shoe->next;
+		hare = (hare->next)->next;
+	}
+	return (0);
+}
+/**
+ * print_listint_safe - Prints a listint_t list safely.
+ * @head: A pointer to the head of the listint_t list.
+ * Return: The number of nodes in the list.
+ * node: consists of two data types in a struct node
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t nodes, index = 0;
+
+	nodes = looped_listint_len(head);
+	if (nodes == 0)
+	{
+		for (; head != NULL; nodes++)
+		{
+			printf("[%p] %d\n", (void *)head, head->n);
+				head = head->next;
+		}
+	}
+	else
+	{
+		for (index = 0; index < nodes; index++)
+		{
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
+		}
+		printf("-> [%p] %d\n", (void *)head, head->n);
+	}
+	return (nodes);
+}
